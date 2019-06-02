f7f69c9e61cece7045c79520ac5aceed2c2b9ec8
Mateusz (@pingwin) <grabekm90@users.noreply.github.com>
2019-06-02 14:09:06 +0200

    CONCOURSE_VERSION as a docker image build arg

--- s/build_and_push_docker_images.sh
+++ s/build_and_push_docker_images.sh
@@ -6,7 +6,9 @@ TAG=$CONCOURSE_VERSION
 test $TAG
 test $PRIVATE_REPOSITORY
 
-docker build -f dockerfiles/concourse-web -t ${PRIVATE_REPOSITORY}/concourse-web:$TAG .
-docker build -f dockerfiles/concourse-worker -t ${PRIVATE_REPOSITORY}/concourse-worker:$TAG .
-docker push ${PRIVATE_REPOSITORY}/concourse-web:$TAG 
-docker push ${PRIVATE_REPOSITORY}/concourse-worker:$TAG 
+docker build -f dockerfiles/concourse-web -t ${PRIVATE_REPOSITORY}/concourse-web:$TAG \
+    --build-arg CONCOURSE_VERSION=$CONCOURSE_VERSION .
+docker build -f dockerfiles/concourse-worker -t ${PRIVATE_REPOSITORY}/concourse-worker:$TAG \
+    --build-arg CONCOURSE_VERSION=$CONCOURSE_VERSION .
+docker push ${PRIVATE_REPOSITORY}/concourse-web:$TAG
+docker push ${PRIVATE_REPOSITORY}/concourse-worker:$TAG
