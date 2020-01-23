a77ef57aca614367d3a5348f29bcfcf34ea701d8
Mateusz (@pingwin) <grabekm90@users.noreply.github.com>
2020-01-23 13:56:02 +0100

    Add a letsencrypt service and a load balancer with the certificate

--- s/prod-compose-up.sh
+++ s/prod-compose-up.sh
@@ -5,10 +5,27 @@ source load_rancher_env.sh
 
 STACK_NAME=${RANCHER_STACK_NAME:-concourse}
 
+disable_https () {
+	sed -i -e "/certs:/,+1d" -e "/default_cert:/d" -e "/protocol: https/,+3d" rancher-compose.yml
+}
+
+enable_https () {
+	# revert any changes
+	git checkout rancher-compose.yml
+}
+
+# disable https protocol if the load balancer is not running yet
+# (acme challenge could not be passed yet and the certificate is not ready)
+rancher ps $STACK_NAME/lb || { disable_https && DISABLED_HTTPS=1 && trap enable_https EXIT; }
+
 rancher \
 	--file docker-compose.yml \
 	--file docker-compose.rancher.yml \
-	up -d \
+	up -d --pull --upgrade --confirm-upgrade \
 	--env-file .env \
 	--stack $STACK_NAME \
 	--description "Concourse continuous integration tool"
+
+if [ "$DISABLED_HTTPS" = 1 ]; then
+	echo "Waiting for the SSL certificate. Please deploy lb once again when it is ready"
+fi
